92ef748a4bc384d57ff71e83740d9c61619ff601
Harpreet Matharu <harp14@hotmail.com>
2020-08-01 23:20:31 +0100

    Created polymorphism file

---
+++ polymorphism.py
@@ -0,0 +1,28 @@
+# Polymorphism: Poly - Many, morph - forms. Polymorphism means many forms
+
+from abc import ABC, abstractmethod
+
+
+class UIControl(ABC):
+    @abstractmethod
+    def draw(self):
+        pass
+
+
+class TextBox(UIControl):
+    def draw(self):
+        print("TextBox")
+
+
+class DropDownList(UIControl):
+    def draw(self):
+        print("DropDownList")
+
+
+def draw(controls):
+    for control in controls:
+        control.draw()
+
+
+ddl = DropDownList()
+draw(ddl)
